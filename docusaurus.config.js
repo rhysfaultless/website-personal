f0a0a7f354173d690261fa9593cc35f45093b571
Rhys Faultless <rhysfaultless@gmail.com>
2022-05-22 17:11:27 -0400

    reverted to old URL to fix build issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Rhys Faultless',
-  url: '/',
+  url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
