1afe9f9a80b0362ba92fcf05942f6150b196dc98
Rhys Faultless <rhysfaultless@gmail.com>
2022-05-22 16:52:24 -0400

    changed tab name, homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,27 +6,12 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-      </div>
-    </header>
-  );
-}
-
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Home`}
       description="Description will go into a meta tag in <head />">
-      {/*  <HomepageHeader />  */}
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
