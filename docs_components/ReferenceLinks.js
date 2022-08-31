ef5fda541b314bcd3e149ec3d417fab23ee4098a
Rhys Faultless <rhysfaultless@gmail.com>
2022-08-31 08:58:26 -0400

    React component for formatting References

---
+++ docs_components/ReferenceLinks.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ReferenceLink(props){
+  const index = props.index;
+  const hyperlink = props.hyperlink;
+  const author = props.author
+  const year = props.year;
+  const title = props.title;
+  return(
+    <p>[<a href={hyperlink}>{index}]</a>, {author}, {year}, {title}</p>
+  )
+}
+
+export default ReferenceLink;
