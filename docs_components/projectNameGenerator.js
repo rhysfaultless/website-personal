bd77187c9b2b1010d2d2b7cf8d5273bef8b0214a
Rhys Faultless <rhysfaultless@gmail.com>
2024-01-01 19:13:19 -0500

    Created component, page, and demo word data.

---
+++ docs_components/projectNameGenerator.js
@@ -0,0 +1,26 @@
+//  docs_components/projectNameGenerator.js
+
+import { React, useState } from "react";
+import words_list from "@site/static/words/words.json"
+
+export default function ProjectNameGenerator() {
+  const length_of_words_list = words_list.words.length;
+  const [firstWord, changeFirstWord] = useState(words_list.words[randomIntegerWithinWordsListLength()].word);
+  const [secondWord, changeSecondWord] = useState(words_list.words[randomIntegerWithinWordsListLength()].word);
+  
+  function randomIntegerWithinWordsListLength() {
+    return Math.floor(Math.random() * length_of_words_list);
+  }
+  
+  function helperProjectNameGenerator() {
+      changeFirstWord(words_list.words[randomIntegerWithinWordsListLength()].word);
+      changeSecondWord(words_list.words[randomIntegerWithinWordsListLength()].word);
+  }
+
+  return (
+      <div className="name-generator">
+          <button onClick={helperProjectNameGenerator}>Name Generator</button>
+          <p>{firstWord}{secondWord}</p>
+      </div>
+  )
+}
