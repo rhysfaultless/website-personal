d9ad0e5e2292a2bd909e73756cbc670dd2687d84
Rhys Faultless <rhysfaultless@gmail.com>
2024-01-06 09:40:48 -0500

    Simplified JSON structure, and added words.

--- docs_components/projectNameGenerator.js
+++ docs_components/projectNameGenerator.js
@@ -5,16 +5,16 @@ import words_list from "@site/static/words/words.json"
 
 export default function ProjectNameGenerator() {
   const length_of_words_list = words_list.words.length;
-  const [firstWord, changeFirstWord] = useState(words_list.words[randomIntegerWithinWordsListLength()].word);
-  const [secondWord, changeSecondWord] = useState(words_list.words[randomIntegerWithinWordsListLength()].word);
+  const [firstWord, changeFirstWord] = useState(words_list.words[randomIntegerWithinWordsListLength()]);
+  const [secondWord, changeSecondWord] = useState(words_list.words[randomIntegerWithinWordsListLength()]);
   
   function randomIntegerWithinWordsListLength() {
     return Math.floor(Math.random() * length_of_words_list);
   }
   
   function helperProjectNameGenerator() {
-      changeFirstWord(words_list.words[randomIntegerWithinWordsListLength()].word);
-      changeSecondWord(words_list.words[randomIntegerWithinWordsListLength()].word);
+      changeFirstWord(words_list.words[randomIntegerWithinWordsListLength()]);
+      changeSecondWord(words_list.words[randomIntegerWithinWordsListLength()]);
   }
 
   return (
