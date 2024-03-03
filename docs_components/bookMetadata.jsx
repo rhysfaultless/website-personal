3ebff435bd9ed6d108459b23c67250f6a7ceec34
Rhys Faultless <rhysfaultless@gmail.com>
2024-03-03 11:38:49 -0500

    Added book metadata

---
+++ docs_components/bookMetadata.jsx
@@ -0,0 +1,24 @@
+//  docs_components/bookMetadata.jsx
+//  props
+//    author
+//    asin
+//    datePublished
+
+import Admonition from "@theme/Admonition";
+
+export default function BookMetadata(props) {
+  const amazon_link = "https://www.amazon.com/dp/" + props.asin
+  return (
+    <Admonition
+      type="caution"
+      icon=" "
+      title="Metadata"
+    >
+      <ul>
+        <li>Author: <em>{props.author}</em></li>
+        <li>Publication Date: <em>{props.datePublished}</em></li>
+        <li>Amazon: <a href={amazon_link} target="_blank"><em>{props.asin}</em></a></li>
+      </ul>
+    </Admonition>
+  )
+}
